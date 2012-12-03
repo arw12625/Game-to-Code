c1672ac58705e0b1f72efd4c2f9bf5baa3d8c66a
Andrew Wintenberg <awintenb@gmail.com>
2012-12-02 21:39:10 -0500

    Updated docs.

--- docs/runtime.js
+++ docs/runtime.js
@@ -2,7 +2,10 @@ function loadintro(name) {
 	document.location = name + '.html';
 }
 function loadlevel(name) {
-	document.location = 'runtime.html'
-	loc = name;
-	document.getElementById('level').location = name + '_level.html';
+	document.location = 'runtime.html';
+}
+function runcode() {
+	var code = window.Blockly.Generator.workspaceToCode('JavaScript');
+	alert(code);
+	eval(code);
 }
