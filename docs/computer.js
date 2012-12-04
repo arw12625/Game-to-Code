40e3385fbefaf8ae7ca6ad2905f56edae6cc7248
Zarriak <parsley.kyle@gmail.com>
2012-12-04 13:09:01 -0500

    Added Variables
    The code makes the program not able to run, so I added it to the current
    Blockly javascript file, and commented out the code.

--- docs/computer.js
+++ docs/computer.js
@@ -14,3 +14,48 @@ Blockly.Language.text_lengthmy = {
 function showhelp() {
 	alert("SUP");
 }
+
+/*Blockly.Language.variables_get = {
+  category: null,
+  helpUrl: 'http://en.wikipedia.org/wiki/Variable_%28computer_science%29',
+  init: function() {
+    this.setColour(330);
+    this.appendDummyInput()
+        .appendTitle(Blockly.LANG_VARIABLES_GET_TITLE)
+        .appendTitle(new Blockly.FieldVariable(
+        Blockly.LANG_VARIABLES_GET_ITEM), 'VAR');
+    this.setOutput(true, null);
+    this.setTooltip(Blockly.LANG_VARIABLES_GET_TOOLTIP);
+  },
+  getVars: function() {
+    return [this.getTitleValue('VAR')];
+  },
+  renameVar: function(oldName, newName) {
+    if (Blockly.Names.equals(oldName, this.getTitleValue('VAR'))) {
+      this.setTitleValue(newName, 'VAR');
+    }
+  }
+};
+
+Blockly.Language.variables_set = {
+  category: null,
+  helpUrl: 'http://en.wikipedia.org/wiki/Variable_%28computer_science%29',
+  init: function() {
+    this.setColour(330);
+    this.appendValueInput('VALUE')
+        .appendTitle(Blockly.LANG_VARIABLES_SET_TITLE)
+        .appendTitle(new Blockly.FieldVariable(
+        Blockly.LANG_VARIABLES_SET_ITEM), 'VAR');
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setTooltip(Blockly.LANG_VARIABLES_SET_TOOLTIP);
+  },
+  getVars: function() {
+    return [this.getTitleValue('VAR')];
+  },
+  renameVar: function(oldName, newName) {
+    if (Blockly.Names.equals(oldName, this.getTitleValue('VAR'))) {
+      this.setTitleValue(newName, 'VAR');
+    }
+  }
+};*/
